2deffd13aae525b4aa22183e5ed156387c8ee782
codeamin11 <legacyofalamin@gmail.com>
2024-03-25 21:19:59 +0600

    react router tailwind css and daisyui added to the project

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,10 @@
+
+const Root = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">this is from root file</h1>
+        </div>
+    );
+};
+
+export default Root;
