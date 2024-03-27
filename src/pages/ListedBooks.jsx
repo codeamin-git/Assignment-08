074ae562bfedf4e6e1170acc87be5fb26f7b3481
codeamin11 <legacyofalamin@gmail.com>
2024-03-27 17:27:52 +0600

    sorting functionality added

--- src/pages/ListedBooks.jsx
+++ src/pages/ListedBooks.jsx
@@ -5,19 +5,15 @@ import { getReadBook, getWishList } from '../utility/localSotrage';
 import ReadList from '../components/ReadList/ReadList';
 import WishList from '../components/WishList/WishList';
 import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import sortBy from 'sort-by';
 const ListedBooks = () => {
-    const [books, setBooks] = useState([])
+    const books = useLoaderData()
     const [readBooks, setReadBooks] = useState([])
     const [wishListBooks, setWishListBooks] = useState([])
+    const [sortBy, setSortBy] = useState("")
 
     useEffect(() => {
-        fetch('/books.json')
-            .then(res => res.json())
-            .then(data => setBooks(data))
-    }, [])
-
-    useEffect(() => {
-        console.log(books);
         
         const storedReadBooks = getReadBook().map(id => parseInt(id));
 
@@ -29,8 +25,26 @@ const ListedBooks = () => {
         setWishListBooks(markedWishListBooks)
     }, [books])
 
-    console.log(readBooks);
-    console.log(wishListBooks);
+    const sortedBooks = (books, sortBy) => {
+        switch (sortBy) {
+            case "rating":
+                return books.sort((a, b) => b.rating - a.rating);
+            case "pages":
+                return books.sort((a, b)=> b.totalPages - a.totalPages);
+            case "year":
+                return books.sort((a, b)=> b.yearOfPublishing - a.yearOfPublishing);
+            default:
+                return books
+        }
+    }
+
+    const handleSortBy = (e) => {
+        setSortBy(e)
+    }
+    
+    const sortedReadBooks = sortedBooks(readBooks, sortBy).reverse();
+    const sortedWishListBooks = sortedBooks(wishListBooks, sortBy).reverse()
+
     return (
         <div className='space-y-20'>
             <div className='text-center'>
@@ -39,8 +53,9 @@ const ListedBooks = () => {
                     <details className="dropdown">
                         <summary className="m-1 btn bg-[#23BE0A] text-white">Sort By<FaAngleDown /></summary>
                         <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li><a>Item 2</a></li>
+                            <li><a onClick={()=>handleSortBy("rating")}>Rating</a></li>
+                            <li><a onClick={()=>handleSortBy("pages")}>Number of Pages</a></li>
+                            <li><a onClick={()=>handleSortBy("year")}>Published Year</a></li>
                         </ul>
                     </details>
                 </div>
@@ -57,14 +72,14 @@ const ListedBooks = () => {
                 <TabPanel>
                     <div className='space-y-4'>
                         {
-                            readBooks.map(book => <ReadList key={book.bookId} book={book}></ReadList>)
+                            sortedReadBooks.map(book => <ReadList key={book.bookId} book={book}></ReadList>)
                         }
                     </div>
                 </TabPanel>
                 <TabPanel>
                     <div>
                         {
-                            wishListBooks.map(book => <WishList key={book.bookId} book={book}></WishList>)
+                            sortedWishListBooks.map(book => <WishList key={book.bookId} book={book}></WishList>)
                         }
                     </div>
                 </TabPanel>
